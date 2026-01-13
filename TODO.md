dbd0d2084824bb408194035f9ed33a7a7681eb3f
djaggi <jagg@zhaw.ch>
2026-01-13 16:20:10 +0100

    add TODO.md

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Todos
+
+- [ ] Propagete two versions of models through the pipeline. Deep think and shallow think.
--- heidi/default_config.py
+++ heidi/default_config.py
@@ -4,6 +4,6 @@ DEFAULT_CONFIG = {
     "project_dir": os.path.abspath(os.path.join(os.path.dirname(__file__), ".")),
     "llm_provider": "anthropic", # "gemini",
     "deep_think_llm": "claude-haiku-4-5",
-    "fast_think_llm": "claude-haiku-4-5",
+    "shallow_think_llm": "claude-haiku-4-5",
     "tickers": "data/test_tickers.txt"
 }
